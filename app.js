456211239259d96c2f7f6405b46d08b65b211050
IcySpar <victorasielrodriguez@gmail.com>
2025-02-20 08:28:36 -0800

    feat: add css styling for home design prototype
    Added ability for server to serve static files/css
    included partials directory in views

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 const indexRouter = require('./routes/indexRouter');
 
