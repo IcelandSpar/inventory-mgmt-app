360a267a782d337ee9c8f751ce133688c32549cd
IcySpar <victorasielrodriguez@gmail.com>
2025-02-25 13:11:43 -0800

    feat: Add express validation when adding books
    Error messages are logged to form page, added CSS
    styles to the error partial.
    Previous info from failed request will also be
    populated onto form for ease of use.
    Error thrown in console.

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,5 +1,20 @@
 const db = require('../db/queries');
+const { body, validationResult } = require('express-validator');
 
+const validateUser = [
+    body('title').trim()
+    .isAlpha().withMessage('Title name must be an alphanumeric value.'),
+    body('author').trim()
+    .isAlpha().withMessage('Author name must be an alphanumeric value.'),
+    body('publisher').trim()
+    .isAlpha().withMessage('Publisher name must be an alphanumeric value.'),
+    body('quantity').trim()
+    .isNumeric().withMessage('Quantity must be a number.')
+    .isFloat({min: 0}).withMessage('Quantity must be a positive number.')
+    .custom(value => value % 1 == 0 ? true : false).withMessage('Quantity must be a whole number, not a decimal.'),
+    body('imageUrl').trim()
+    .isURL().withMessage('Image submission must be an ImageURL')
+];
 
 const getIndexPage =  async (req, res) => {
     const data = await db.getAllBookInfo();
@@ -18,13 +33,21 @@ const postBookOrVideoGame = async (req, res) => {
     }
 };
 
-const postAddBook = async (req, res) => {
+const postAddBook = [
+    validateUser,
+    async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        
+        return res.status(400).render('add-book-form', {errors: errors.array(), data: req.body});
+    }
+
     const { title, author, publisher, quantity, description, imageUrl } = req.body;
     const newBookArr = [title, author, publisher, quantity, description, imageUrl];
     await db.postBook(newBookArr);
     
     res.redirect('/');
-};
+}]
 
 const getAddBookForm = (req, res) => {
     res.render('add-book-form');
@@ -37,7 +60,7 @@ const getEditBookForm = async (req, res) => {
 }
 
 const postEditBookForm = async (req, res) => {
-    db.updateBookDetails(req.params.id, [req.body.title, req.body.author, req.body.publisher, req.body.quantity, req.body.description, req.body.imageUrl])
+    await db.updateBookDetails(req.params.id, [req.body.title, req.body.author, req.body.publisher, req.body.quantity, req.body.description, req.body.imageUrl]);
     res.redirect('/');
 }
 
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,7 +2,6 @@ const { Router } = require('express');
 
 const indexRouter = Router();
 const { getIndexPage, getCreateForm, postBookOrVideoGame, getAddBookForm, postAddBook, getEditBookForm, postEditBookForm } = require('../controllers/indexController');
-const { body } = require('express-validator');
 
 
 indexRouter.get('/', getIndexPage);
@@ -12,8 +11,8 @@ indexRouter.post('/create', postBookOrVideoGame);
 indexRouter.get('/create/add-book', getAddBookForm);
 indexRouter.post('/create/add-book', postAddBook);
 
-indexRouter.get('/editBook/:id', getEditBookForm);
-indexRouter.post('/editBook/:id', postEditBookForm);
+indexRouter.get('/editBook/:title/:id', getEditBookForm);
+indexRouter.post('/editBook/:title/:id', postEditBookForm);
 
 indexRouter.get('/create/add-videogame', (req, res) => {
     res.end('Add videogame Form');
